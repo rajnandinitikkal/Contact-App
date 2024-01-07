1302cce738f4cf7a00d8e3bbae6474977a4996d9
rajnandinitikkal <rajnandinitikkal@gmail.com>
2024-01-07 12:19:33 +0530

    changes in object

--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -1,189 +1,189 @@
-import React,{useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 import ContactCard from '../../Component/ContactCard/ContactCard';
 import showToast from 'crunchy-toast';
 
 export default function Home() {
-     
-    const [contacts , setContacts] = useState([
+
+    const [contacts, setContacts] = useState([
         {
-            name:'Rutuja Gaikwad',
-        mobile: '9158569290',
-        email:'gaikwadrutu@gmail.com'
+            name: 'Rutuja Tikkal',
+            mobile: '9158569290',
+            email: 'Tikkalrutu@gmail.com'
         },
         {
-            name:'Sakshi Bodakhe',
+            name: 'Sakshi Bodakhe',
             mobile: '9023601272',
-            email:'sakshi83@gmail.com'
+            email: 'sakshi83@gmail.com'
         },
         {
-            name:'Swara Gaikwad',
+            name: 'Swara Gaikwad',
             mobile: '9122608934',
-            email:'gaikwadswara@gmail.com'
+            email: 'gaikwadswara@gmail.com'
         },
     ]);
-    const [name , setName] = useState('')
-    const [email , setEmail] = useState('')
-    const [mobile , setMobile] = useState('')
-    const [editIndex , setEditIndex] = useState('-1')
-    const [isEditMode , setIsEditMode] = useState(false)
-    
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [mobile, setMobile] = useState('')
+    const [editIndex, setEditIndex] = useState('-1')
+    const [isEditMode, setIsEditMode] = useState(false)
+
     const addContacts = () => {
-    
-    if(!name){
-        showToast('Please enter name','error',3000)
-        return;
-    }
-    if(!email){
-        showToast('please enter email','error',3000)
-        return;
-    }
-    if(!mobile){
-        showToast('Please enter mobile number','error',3000)
-        return;
-    }   
-       const obj = {
-        name: name,
-        email:email,
-        mobile: mobile
-       }
 
-       const newContacts = [...contacts,obj];
-       setContacts([newContacts]);
-       saveToLocalStorage(newContacts);
+        if (!name) {
+            showToast('Please enter name', 'error', 3000)
+            return;
+        }
+        if (!email) {
+            showToast('please enter email', 'error', 3000)
+            return;
+        }
+        if (!mobile) {
+            showToast('Please enter mobile number', 'error', 3000)
+            return;
+        }
+        const obj = {
+            name: name,
+            email: email,
+            mobile: mobile
+        }
 
-       showToast('Contact Added Sucessfully','success',3000)
+        const newContacts = [...contacts, obj];
+        setContacts([newContacts]);
+        saveToLocalStorage(newContacts);
 
-       setName('');
-       setEmail('');
-       setMobile('');
+        showToast('Contact Added Sucessfully', 'success', 3000)
+
+        setName('');
+        setEmail('');
+        setMobile('');
     };
 
-    const deleteContact = (mobileNumber)=>{
+    const deleteContact = (mobileNumber) => {
         let indexToDelete = -1;
 
-        contacts.forEach((contactDetail,index) =>{
-            if(contactDetail.mobile === mobileNumber){
+        contacts.forEach((contactDetail, index) => {
+            if (contactDetail.mobile === mobileNumber) {
                 indexToDelete = index;
             }
         })
 
-        contacts.splice(indexToDelete,1);
+        contacts.splice(indexToDelete, 1);
 
         saveToLocalStorage(contacts);
 
         setContacts([...contacts])
 
-        showToast('Contact Deleted Sucessfully','success',3000)
+        showToast('Contact Deleted Sucessfully', 'success', 3000)
     }
 
     const saveToLocalStorage = (contactsData) => {
-        localStorage.setItem('contacts',JSON.stringify(contactsData))
+        localStorage.setItem('contacts', JSON.stringify(contactsData))
     }
 
-    const loadFromLocalStorage = () =>{
+    const loadFromLocalStorage = () => {
         const contactsData = JSON.parse(localStorage.getItem('contacts'));
 
-        if(contactsData){
+        if (contactsData) {
             setContacts(contactsData);
         }
     }
 
-    const enableEditMode = (index)=>{
-       const contactsData = contacts[index];
+    const enableEditMode = (index) => {
+        const contactsData = contacts[index];
 
-       setName(contactsData.name);
-       setEmail(contactsData.email);
-       setMobile(contactsData.mobile);
+        setName(contactsData.name);
+        setEmail(contactsData.email);
+        setMobile(contactsData.mobile);
 
-       setEditIndex(index);
-       setIsEditMode(true);
+        setEditIndex(index);
+        setIsEditMode(true);
     }
 
-    const editContact = ()=>{
-       const obj = {
-        name : name,
-        email : email,
-        mobile : mobile
-       }
+    const editContact = () => {
+        const obj = {
+            name: name,
+            email: email,
+            mobile: mobile
+        }
 
-       contacts[editIndex] = obj;
-        
-       setContacts([...contacts]);
+        contacts[editIndex] = obj;
 
-       saveToLocalStorage(contacts);
+        setContacts([...contacts]);
 
-       showToast('Contact edited successfully','success',3000)
+        saveToLocalStorage(contacts);
 
-       setName('');
-       setEmail('');
-       setMobile('');
+        showToast('Contact edited successfully', 'success', 3000)
 
-       setIsEditMode(false);
+        setName('');
+        setEmail('');
+        setMobile('');
+
+        setIsEditMode(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         loadFromLocalStorage()
     })
-  return (
-   <>
-   <h1 className='app-title'>📞Contact App</h1>
+    return (
+        <>
+            <h1 className='app-title'>📞Contact App</h1>
 
-   <div className='app-body'>
+            <div className='app-body'>
 
-    <div className='contact-container'>
-    <h2 className='sub-heading'>Show contacts</h2>
-    {
-        contacts.map((contacts,index)=>{
-           return(
-            <>
-            <ContactCard key={index} 
-            name={contacts.name} 
-            mobile={contacts.mobile} 
-            email={contacts.email}
-            deleteContact={deleteContact}
-            enableEditMode={enableEditMode}
-            index={index}/>
-            </>
-           )
-        })
-    }
-    </div>
+                <div className='contact-container'>
+                    <h2 className='sub-heading'>Show contacts</h2>
+                    {
+                        contacts.map((contacts, index) => {
+                            return (
+                                <>
+                                    <ContactCard key={index}
+                                        name={contacts.name}
+                                        mobile={contacts.mobile}
+                                        email={contacts.email}
+                                        deleteContact={deleteContact}
+                                        enableEditMode={enableEditMode}
+                                        index={index} />
+                                </>
+                            )
+                        })
+                    }
+                </div>
 
-    <div  className='add-contact-container'>
-    <h2 className='sub-heading'>
-        { isEditMode ? 'Edit Contact' : 'Add Contact'}</h2>
-    <form>
-        <input type="text" placeholder='name' className='user-input' 
-        onChange={(e)=>{
-            setName(e.target.value);
-        }}
-        value={name}
-        ></input>
+                <div className='add-contact-container'>
+                    <h2 className='sub-heading'>
+                        {isEditMode ? 'Edit Contact' : 'Add Contact'}</h2>
+                    <form>
+                        <input type="text" placeholder='name' className='user-input'
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+           y                 value={name}
+                        ></input>
 
-        <input type="email" placeholder='Email' className='user-input' 
-         onChange={(e)=>{
-            setEmail(e.target.value);
-        }}
-        value={email}
-        ></input>
+                        <input type="email" placeholder='Email' className='user-input'
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
+                            value={email}
+                        ></input>
 
-        <input type="tel" placeholder='Contact' className='user-input' 
-         onChange={(e)=>{
-            setMobile(e.target.value);
-        }}
-        value={mobile}
-        ></input>
+                        <input type="tel" placeholder='Contact' className='user-input'
+                            onChange={(e) => {
+                                setMobile(e.target.value);
+                            }}
+                            value={mobile}
+                        ></input>
 
-        <button type="button" className='btn-add-contacts' onClick={()=>{
-            isEditMode ? editContact():addContacts()
-        }
-        }> { isEditMode ? 'Edit Contact' : 'Add Contact'}</button>
-    </form>
-    </div>
-   </div>
-   
-   
-   </>
-  )
+                        <button type="button" className='btn-add-contacts' onClick={() => {
+                            isEditMode ? editContact() : addContacts()
+                        }
+                        }> {isEditMode ? 'Edit Contact' : 'Add Contact'}</button>
+                    </form>
+                </div>
+            </div>
+
+
+        </>
+    )
 }
